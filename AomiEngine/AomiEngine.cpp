368a5bfdff1a6f1731f3fae0ea5d2c16bacdc720
minamiaomi <minamiaomi@LAPTOP-1K3GK5JA>
2023-06-09 17:29:44 +0900

    aomiEngine

--- AomiEngine/AomiEngine.cpp
+++ AomiEngine/AomiEngine.cpp
@@ -1,5 +1,8 @@
 #include "AomiEngine.hpp"
 
+#include "AomiEngineUtils/Assert.hpp"
+#include "AomiEngineUtils/Bitset.hpp"
+
 namespace AomiEngine {
 
 	int Main() {
---
+++ AomiEngine/Graphics.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace AomiEngine {
+
+	class Graphics {
+	public:
+
+	private:
+
+	};
+
+} // AomiEngine
