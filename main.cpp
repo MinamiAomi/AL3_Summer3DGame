2f4533b25885c8f6ce35702493bf63b1dea7456d
MinamiAomi <k022g0040@g.neec.ac.jp>
2023-08-03 23:32:39 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
 #include <Windows.h>
 
-#include "AomiEngine.hpp"
-
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	return AomiEngine::Main();
+
+
+
+
+    return 0;
 }
